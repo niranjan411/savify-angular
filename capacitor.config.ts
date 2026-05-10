992cb7893f860c3cafb6efa8838f072c09b3ac5b
niranjan411 <niranjanmemane47@gmail.com>
2026-05-10 20:01:09 +0530

    7 commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.savify.app',
   appName: 'savify',
-  webDir: 'dist/savify'
+  webDir: 'dist/savify/browser'
 };
 
 export default config;
